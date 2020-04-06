7937308b19eb16472d2ba2758aa57f371ec37af9
Blake Rogan <blake.rogan@readify.net>
2020-04-06 14:44:12 +1000

    Fix MockDatabase.ts and relocate it

---
+++ apollo/src/data/MockDatabase.ts
@@ -0,0 +1,43 @@
+import {IDatabase} from "./IDatabase";
+import {Book} from "../types/book";
+
+class MockDatabase implements IDatabase {
+    private static readonly bookCollection: Book[] = [
+        {
+            title: 'Harry Potter and the Chamber of Secrets',
+            author: 'J.K. Rowling',
+        },
+        {
+            title: 'Jurassic Park',
+            author: 'Michael Crichton',
+        },
+    ];
+
+    book(title: string): Book | null {
+        return MockDatabase.bookCollection.find((book) => {
+            if (book.title === title) {
+                return book;
+            }
+            return undefined;
+        }) || null;
+    }
+
+    books(): Book[] {
+        return MockDatabase.bookCollection;
+    }
+
+    insertBook(title: string, author: string): boolean {
+        if (this.book(title) !== null) {
+            return false;
+        }
+        return MockDatabase.bookCollection.push({
+            title,
+            author
+        }) !== 0;
+    }
+
+}
+
+export {
+    MockDatabase,
+};
